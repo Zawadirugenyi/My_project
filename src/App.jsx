113d259e7f468459d959316a3a6915718be0b97f
zawadirugenyi <zawadirugenyi@gmail.com>
2024-01-07 16:13:50 +0300

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
+
+import "./assets/css/bootstrap.min.css"
 import "./assets/css/all.css"
-
-import "./assets/css/animate.css"
-import "./assets/css/nice-select.css"
-
-import "./assets/css/swiper-bundle.min.css"
+import "./assets/css/magnific-popup.css"
 import "./assets/css/meanmenu.min.css"
 import "./assets/sass/style.css"
 
 
-import { Preload } from "./Preload"
-import Navbar from "./navbar"
-import Bunner from "./Bunner"
-import Accommodation from "./Accommodation"
-import Deluxearea from "./Deluxearea"
-import Services from "./Services"
-import Testimonial from "./Testimonial"
-import Footer from "./Footer"
-import Scoll from "./Scroll"
+import "./assets/js/bootstrap.min.js"
+import "./assets/js/swiper-bundle.min.js"
+import "./assets/js/isotope.pkgd.min.js"
+
+
+
+import { Preload } from "./componets/Preload"
+import Navbar from "./componets/Navbar"
+import Bunner from "./componets/Bunner"
+import Accommodation from "./componets/Accommodation"
+import Deluxearea from "./componets/Deluxearea"
+import Services from "./componets/Services"
+import Testimonial from "./componets/Testimonial"
+import Footer from "./componets/Footer"
+import Scoll from "./componets/Scroll"
 export default function App(){
 
   return (
